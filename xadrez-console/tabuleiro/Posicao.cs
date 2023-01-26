15af9b444f0b700426749bf00a902f9cea2594a2
Lara Gabriele <laragboliver@gmail.com>
2023-01-26 15:01:50 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace tabuleiro
+{
+    public class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao( int linha, int coluna )
+        {
+            this.coluna = coluna; 
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return linha 
+                + ", " 
+                + coluna;
+        }
+    }
+}
